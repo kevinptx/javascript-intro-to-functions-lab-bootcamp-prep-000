c2679d5c943892efe19e3d39465fa1980c309550
Kevin Peery <kevin.peery@gmail.com>
2018-03-16 04:45:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string === string.toLowerCase())
-  return `I can't hear you!" if ${string} is lowercase`
+  return `I can't hear you! if ${string} is lowercase`
 }
